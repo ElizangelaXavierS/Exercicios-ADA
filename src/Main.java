e7d85724591bec2a4c39f8e16090b7bb0c74ac0d
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-07-26 16:21:20 -0300

    Arraycando conceitos de   descondicionais e operadores

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import lista3.Solution;
 
 public class Main {
     public static void main(String[] args) {
-        double[] input = {20.0, 80.0, 60.0, 40.0};
-        System.out.println(Solution.geraRecibo(input));
+        //double[] input = {20.0, 80.0, 60.0, 40.0};
+        //System.out.println(Solution.geraRecibo(input));
+
+       int[] input = {3,3,5};
+        System.out.println(Solution.eUmTriangulo(input));
     }
 }
--- src/lista3/Solution.java
+++ src/lista3/Solution.java
@@ -6,8 +6,6 @@ public class Solution {
     public static String geraRecibo(double[] input) {
         double totalSemDesconto = 0;
         double valorDoDesconto = 0;
-        double totalAPagar = 0;
-
 
         Arrays.sort(input);
 
@@ -15,18 +13,22 @@ public class Solution {
             totalSemDesconto += input[i];
         }
 
-        for (int i = 0; i < input.length/2; i++) {
+        for (int i = 0; i < input.length / 2; i++) {
             valorDoDesconto += input[i] / 2;
         }
 
-        totalAPagar = totalSemDesconto - valorDoDesconto;
-
+        double totalAPagar = totalSemDesconto - valorDoDesconto;
 
         return "Valor total:" + totalSemDesconto + " | Valor de descontos: " + valorDoDesconto + " | Valor a pagar: " + totalAPagar;
     }
 
     public static boolean eUmTriangulo(int[] input) {
-        return false;
+
+        if (input[0] < (input[1] + input[2]) && input[1] < (input[0] + input[2]) && input[2] < (input[1] + input[0])) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
