39d23c0e0ece43a4ebd5e935e39ddc58faf0608c
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-07-28 11:17:23 -0300

    Praticando conceitos de estruturas condicionais e repetiçÃão

---
+++ src/lista3/Main.java
@@ -0,0 +1,20 @@
+package lista3;
+
+import lista3.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        double[] input = {20.0, 80.0, 60.0, 40.0};
+//        System.out.println(Solution.geraRecibo(input));
+//
+//        int[] input = {3,3,5};
+//        System.out.println(Solution.eUmTriangulo(input));
+
+//        double[] input = {9.0,9.0,10.0,10.0};
+//        System.out.println(Solution.mediaAproveitamento(input));
+
+        int[] input = {1, 1, 2, 3, 4, 5, 5, 4, 3, 2, 10};
+        System.out.println(Solution.numeroUnico(input));
+}
+    }
--- src/lista3/Solution.java
+++ src/lista3/Solution.java
@@ -30,22 +30,46 @@ public class Solution {
         int diasDeVida = input[0] * 365 + input[1] * 30 + input[2];
         return diasDeVida;
     }
-
+    //double[] input = {2.0, 5.0, 8.0, 6.0};
     public static String mediaAproveitamento(double[] input) {
-         double media_aproveitamento = (input[0]+ input[1]+ input[2] + input[3])/4;
-         String conceito = "";
-         if (media_aproveitamento >= 9.0) {
-             conceito = "A";
-         } else if (media_aproveitamento >= 7.5 && media_aproveitamento < 9.0) {
-             conceito = "B";
-         }else if (media_aproveitamento >= 6.0 &&media_aproveitamento < 7.5) {
-             conceito = "C";
-         }else if (media_aproveitamento < 6.0) {
-             conceito = "D";
-         }else {
-             System.out.println("Media invalida");
-         }
+        double media_aproveitamento = (input[0] + input[1] + input[2] + input[3]) / 4;
+        System.out.println(media_aproveitamento);
+        String conceito = "";
+        if (media_aproveitamento >= 9.0) {
+            conceito = "A";
+        } else if (media_aproveitamento >= 7.5 && media_aproveitamento < 9.0) {
+            conceito = "B";
+        } else if (media_aproveitamento >= 6.0 && media_aproveitamento < 7.5) {
+            conceito = "C";
+        } else if (media_aproveitamento < 6.0) {
+            conceito = "D";
+        } else {
+            System.out.println("Media invalida");
+        }
         return conceito;
+    }
+
+    public static int numeroUnico(int[] input) {
+        boolean unico;
+        int saida = -1;
+        for( int i = 0; i < input.length; i++){
+            unico = true;
+            for ( int j = 0; j < input.length; j++){
+                if(i == j){
+                    continue;
+                }
+                if( input[i] == input[j]){
+                     unico = false;
+                     break;
+                }
+            }
+            if(unico){
+                saida = input[i];
+                break;
+            }
+
+        }
+        return saida;
     }
 }
 
