f52be7c931c12161773b3d1b9c5c77e11c7a01d3
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-08-12 14:02:26 -0300

    Definindo um objeto do tipo pessoa

---
+++ src/lista_de_exercicio_poo/exercicio01/Main.java
@@ -0,0 +1,7 @@
+package lista_de_exercicio_poo.exercicio01;
+
+public class Main {
+    public static void main(String[] args) {
+       // Pessoa pessoa = new Pessoa();
+    }
+}
---
+++ src/lista_de_exercicio_poo/exercicio01/Pessoa.java
@@ -0,0 +1,56 @@
+package lista_de_exercicio_poo.exercicio01;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    double altura;
+    double peso;
+
+    public Pessoa(String nome, int idade, double altura, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void exibirInformacoes (){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Altura: " + this.altura);
+        System.out.println("Peso " + this.peso);
+
+    }
+
+}
