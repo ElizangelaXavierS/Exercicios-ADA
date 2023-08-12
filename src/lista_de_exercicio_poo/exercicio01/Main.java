5e52e41b971ec8054701989354535c0a72d30e53
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-08-12 14:09:28 -0300

    Definindo um objeto do tipo pessoa

--- src/lista_de_exercicio_poo/exercicio01/Main.java
+++ src/lista_de_exercicio_poo/exercicio01/Main.java
@@ -1,7 +1,25 @@
 package lista_de_exercicio_poo.exercicio01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       // Pessoa pessoa = new Pessoa();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int  idade = sc.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura= sc.nextDouble();
+
+        System.out.println("Digite seu peso: ");
+        double peso= sc.nextDouble();
+
+        Pessoa pessoa = new Pessoa(nome,idade, altura,peso);
+
+        pessoa.exibirInformacoes();
     }
 }
