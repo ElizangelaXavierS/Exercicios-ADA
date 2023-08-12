faca610d81c38075aca3535cc946019f19a17de3
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-08-12 14:16:40 -0300

    Definindo um objeto do tipo calculadora

--- src/lista_de_exercicio_poo/exercicio02/Calculadora.java
+++ src/lista_de_exercicio_poo/exercicio02/Calculadora.java
@@ -10,22 +10,6 @@ public class Calculadora {
         this.valor2 = valor2;
     }
 
-    public int getValor1() {
-        return valor1;
-    }
-
-    public void setValor1(int valor1) {
-        this.valor1 = valor1;
-    }
-
-    public int getValor2() {
-        return valor2;
-    }
-
-    public void setValor2(int valor2) {
-        this.valor2 = valor2;
-    }
-
     public int somar(int valor1, int valor2){
         return valor1 + valor2;
 
